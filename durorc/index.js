ecadcd936f1e7351b3107539e3163fb3b80b54c6
msalihcavus <m.salihcavus@hotmail.com>
2022-06-22 11:40:22 +0400

    durable function

---
+++ durorc/index.js
@@ -0,0 +1,18 @@
+﻿const df = require("durable-functions");
+
+module.exports = df.orchestrator(function* (context) {
+    const outputs = [];
+
+    // Replace "Hello" with the name of your Durable Activity Function.
+    outputs.push(yield context.df.callActivity("duract", "Tokyo"));
+    outputs.push(yield context.df.callActivity("duract", "Seattle"));
+    outputs.push(yield context.df.callActivity("duract", "London"));
+
+    
+    //birden fazla activity i orchestrator içinden çağırabiliriz
+    outputs.push(yield context.df.callActivity("duract1", "Seattle"));
+    outputs.push(yield context.df.callActivity("duract2", "London"));
+
+    // returns ["Hello Tokyo!", "Hello Seattle!", "Hello London!"]
+    return outputs;
+});
